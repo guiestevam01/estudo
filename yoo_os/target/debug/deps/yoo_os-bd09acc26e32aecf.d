60f27a8aa4e14b6c24f5b146ef18fa4de30823df
guiestevam01 <guiestara34@gmail.com>
2026-04-16 18:44:27 -0300

    Remove node_modules do controle de versão

--- yoo_os/target/debug/deps/yoo_os-bd09acc26e32aecf.d
+++
@@ -1,5 +0,0 @@
-/home/gui/Documents/estudo/yoo_os/target/debug/deps/yoo_os-bd09acc26e32aecf.d: src/main.rs
-
-/home/gui/Documents/estudo/yoo_os/target/debug/deps/yoo_os-bd09acc26e32aecf: src/main.rs
-
-src/main.rs:
--- yoo_os/target/thumbv7em-none-eabihf/debug/yoo_os.d
+++
@@ -1,1 +0,0 @@
-/home/gui/Documents/estudo/yoo_os/target/thumbv7em-none-eabihf/debug/yoo_os: /home/gui/Documents/estudo/yoo_os/src/main.rs
