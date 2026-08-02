3840052b3e0b3fbdbc84c2c22f4af09cf4f06462
guiestevam01 <guiestara34@gmail.com>
2026-08-02 18:28:09 -0300

    add sobre embedding

---
+++ java/jdbc-study/src/entidade/Automovel.java
@@ -0,0 +1,10 @@
+package Entidade;
+
+public class Automovel {
+    private Long id;
+    private Integer anoFabricacao;
+    private String modelo;
+    private Integer anoModelo;
+    private String marca;
+    private String observacoes;
+}
