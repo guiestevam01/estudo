cf634469ae7d8717c75c8145534c36217d86c917
guiestevam01 <guiestara34@gmail.com>
2026-08-29 20:00:04 -0300

    chore(fundacao): registra alterações automaticamente (2026-08-29 20:00)

--- aprendendo-go/Fundacao/http/teste.go
+++ aprendendo-go/Fundacao/http/teste.go
@@ -5,14 +5,26 @@ import (
 	"io"
 	"net/http"
 )
-//retorna dados e error e para verificar se teve erro
+
+// retorna dados e error e para verificar se teve erro
 func getIssueData() ([]byte, error) {
-	res, err := http.Get("https://api.boot.dev/v1/courses_rest_api/learn-http/issues") // resposta 
+	res, err := http.Get("https://api.boot.dev/v1/courses_rest_api/learn-http/issues") // resposta
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %w", err)
 	}
-	defer res.Body.Close() // defer para garantir que 
+	defer res.Body.Close() // defer para garantir que
 
 	data, err := io.ReadAll(res.Body)
 	return data, err
+}
+func main() {
+	data, err := getIssueData()
+	if err != nil {
+		fmt.Println("Aconteceu um erro:", err)
+		return
+	}
+
+	fmt.Println("Quantidade de bytes recebidos:", len(data))
+	fmt.Println("Resposta da API:")
+	fmt.Println(string(data))
 }
